367aa79604e612cb99d87e6d6ba946dfe3b29fe8
girish-creator <girikripa@yahoo.com>
2022-05-05 10:33:56 +0200

    - updated tests/__init__.py file

--- jenkins-slave-tester1/docker_multi_build/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(os.path.abspath(os.curdir)))
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(os.curdir))))
