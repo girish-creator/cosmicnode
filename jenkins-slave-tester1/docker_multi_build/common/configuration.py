09dee5a3baf5d7dee7a8c1b034704bd54711c6da
girish-creator <girikripa@yahoo.com>
2022-05-05 22:05:05 +0200

    - updated configuration.py set the correct linux com port format

--- jenkins-slave-tester1/docker_multi_build/common/configuration.py
+++ jenkins-slave-tester1/docker_multi_build/common/configuration.py
@@ -2,9 +2,9 @@ import os
 MQTT_SERVER = "mqtt3.cosmicnode.com"
 MQTT_PORT = 8883
 MQTT_KEEP_ALIVE = 60
-SERIAL_RX_COM_PORT = "ttyUSB0"
-SERIAL_TX_COM_PORT = "ttyUSB1"
-SERIAL_NODE_COM_PORT = "ttyUSB2"
+SERIAL_RX_COM_PORT = "/dev/ttyUSB0"
+SERIAL_TX_COM_PORT = "/dev/ttyUSB1"
+SERIAL_NODE_COM_PORT = "/dev/ttyUSB2"
 VOLUME = ""
 ROOT_PATH = os.getcwd()
 #ROOT_PATH = "C:\\Users\\Girish-Nair\\Documents\\GitHub\\cosmicnode\\jenkins-slave-tester1\\docker_multi_build"
